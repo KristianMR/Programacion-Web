425f236da3a47bdd3044352353e9a6b894230508
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-05-23 02:27:59 -0500

    eliminar vulnerabilidad

--- form.py
+++ form.py
@@ -40,7 +40,6 @@ label, input, textarea {
 contact_js = """function enviar() {
     let email = document.getElementById('email').value;
     alert("Gracias por contactarnos " + email);
-    document.getElementById('msg').innerHTML = "<script>alert('XSS en mensaje')</script>";
 }
 """
 
